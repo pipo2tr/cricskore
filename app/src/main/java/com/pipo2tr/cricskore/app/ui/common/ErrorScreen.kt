07ad1f8229ae06a150a0170ceef2e82a25cfccd9
pipo2tr <siddharthpipo2tr@gmail.com>
2023-09-29 18:54:40 +0530

    fix: individual score formatting

--- app/src/main/java/com/pipo2tr/cricskore/app/ui/common/ErrorScreen.kt
+++ app/src/main/java/com/pipo2tr/cricskore/app/ui/common/ErrorScreen.kt
@@ -1,6 +1,6 @@
 package com.pipo2tr.cricskore.app.ui.common
 
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -8,5 +8,5 @@ import androidx.wear.compose.material.Text
 
 @Composable
 fun ErrorScreen(message: String = "An unexpected error occurred") {
-    Text(text = message, Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+    Text(text = message, Modifier.fillMaxSize(), textAlign = TextAlign.Center)
 }
